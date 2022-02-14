e423d15ab9060a727ae7bf9d1101d1579a69a358
Muhammed Abdel Dayem <m.eldeeb2011@yahoo.com>
2022-02-14 09:55:09 +0400

    update

--- lib/api/store_sheets_api.dart
+++ lib/api/store_sheets_api.dart
@@ -13,15 +13,15 @@ class StoreSheetsApi {
   // Credentials
   static const _credentials = r'''{
   "type": "service_account",
-  "project_id": "online-store-340218",
-  "private_key_id": "343484ab04dbfb8f3c0d7f29ca6238c96a223583",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/3y5WGfKZj+po\nMkNrzU2yJkeH0RwSzkuG1mOlJ0JspD6r3NY9LWjI9iH42NgD/G6OH6H0HHSmQt1I\niErf18/+m4F4q/PGm9tTh2r9R1zxNsPmq/j0tZEZC01znr+EnGYyMZRziYah/ltt\nVg9HfN0IIM0W7DPLHzPQRwDNBzGWOxBKgJ60ia1E5YfpKTGkiZke2XduVylTmOG3\n6bnYf07l1EuXD8jNVAyAQcW/IsSq7pnwXGvclQods2nCjCZtJ6dCcdlSfxogyvmR\n0SLE2DMpnEVdisbN3sX8xPNPWISyMA/czM5J7WSWZF9rjiPV/JXoOv8/LaNZrvDv\n/zelanq3AgMBAAECggEABrFUPnCdQ7n/ZS8LpwD8StCHAjfLEOTdQZDM3P/nkz6P\naRNE7dFdrFTZjoKR1mHBwXddANTVRM9kRJlscy5l9O2bsIfe8viQx/MbSkZo+Yj9\ngFhTUMXcj9ETHItXBtslvW55TSZo6dGjgsbTtZynYRcz0C7hB57JAeTYJE6x7hmF\nyp588G/opQ/fLetae7TE27f3dNoTZmDiipFuqPlNljfNJQbVGVeCecV/I18iwics\n/tg28KNJ37CfL/HjlibEbsPIOBk0k9BtGQZt4cvZ6yhw3su48k0x00O5D4CFIuuh\nx9jLx7PAnPjYPdLtUuUVnu0o2aQ7ql8+EOqhGsqNkQKBgQDzRCCtgahec9Wp5fWM\nqHfVsghKStpQJ4E5Yj9UpUkxNJGO/nVXfN1OkhWFNQGeRKZZguaZKiMwqIRp20S2\naLLxiopxM6lQr3hN19C0HQivWY2k1gdt6b+IDkBSoIN/8+A1tkdwDR7H2Z0A12I/\nih5/gB00ieSlyvkszQCLH0/HvQKBgQDJ6ljTFTiJKiUeF4YFZd4vxd2WUFWgAi2A\n+hBjljYNnr1SHd6EG8MjBbFp4I1QXKinrSgg/eXPRPlfQIX6bXWKUfj9he06bzHe\nZ9tgEnN+jGCY//GSAFOjHZpRCSPJAWrfzuIiYZdBf+tLBLLRgK3gNEqceu5cAYGI\nGCqR76IpgwKBgQCRRL2+3w6MWN/e9VQNCw2ErywEMW7tLqocem8XPRzYJWxG1+16\nsTX3FYWEZoLw4E32ZhUOofshs4BfNnFx/FPfqbvCnfceF6rf0WDwLeZh0DillqoZ\nGN0nglsm/HOKlTfu20sNvsgC0BDRz8z75J8RxI+sMCw6VVs3RbRtX+VkgQKBgHqT\nOlMCGe0i2Z+X9jNGsQhdXPSIVkKpr8gRhmt+X9gK2rGEEnYuiFq1dQtqVz9G0Nfh\nxW+p0p3uKFjLycnKM0XCF56IdAg6QpLah5GeAeD8BRQ8pADXE+785JZ3LvMyWhf9\n9seaaQFJT6Sd+JQ4QtXNyNGPyy2teF4PFaC8UbxTAoGAFbrNUOyzOabYInMGqu9r\nhlstlgW1BBZ2ItkqFIzy9cRjY3ZENcrJf4vkxhdN5y4Yay0pFnzvKNAOYEB7hOf5\nxn90+sGH2zI10JDhpooWERamIJ+o1sT+BNwgITrFaNbSwUbjtgfwmLMJ1TU8GChj\nStIlzwflGiNCqS6EVAAPvJA=\n-----END PRIVATE KEY-----\n",
-  "client_email": "sheets-online-store@online-store-340218.iam.gserviceaccount.com",
-  "client_id": "106048012423790589863",
+  "project_id": "XXXXXXXX",
+  "private_key_id": "XXXXXX",
+  "private_key": "XXXXXXXXX",
+  "client_email": "XXXXX",
+  "client_id": "XXXXXXX",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sheets-online-store%40online-store-340218.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/XXXXXXXX"
 }
 ''';
   // Spreadsheet ID
